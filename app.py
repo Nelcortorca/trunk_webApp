57fb6ddc0030e7b48bb28ead77a2b8c1c678da36
Nelcortorca <sato00y3700@gmail.com>
2020-09-27 18:46:27 +0900

    予測の結果を返すように変更

--- app.py
+++ app.py
@@ -5,6 +5,8 @@ import pandas as pd
 import os
 import string
 import copy
+from prediction import pred
+
 glob_file= "XXXX"
 glob_file_name="XXX"
 app = Flask(__name__)
@@ -22,10 +24,12 @@ def result():
     filepath = datetime.now().strftime("%Y%m%d%H%M%S") +".csv"
     file_name=file.filename
     file.save("./static/"+filepath)
+
+
     global glob_file
     global glob_file_name
     glob_file_name =file_name
-    glob_file = filepath
+    glob_file = pred("./static/"+filepath)
 
     return render_template("download_csv.html")
 
@@ -34,10 +38,10 @@ def download():
     ret_filepath = glob_file
     ret_filename=glob_file_name
     response = make_response()
-    response.data = open("./static/"+ret_filepath, "rb").read()
+    response.data = open(ret_filepath, "rb").read()
     response.minetype="text/csv"
     downloadFileName =  ret_filename
-    response.headers['Content-Disposition'] = 'attachment; filename=' + downloadFileName
+    response.headers['Content-Disposition'] = 'attachment; filename=' + "predicted_"+downloadFileName
     return response
 
 
